5b75c664284f59faa7187dc9700578e810eefabf
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-08 10:34:41 +0700

    Initial Commit

---
+++ SwipeActionToAnyList/SwipeActionToAnyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeActionToAnyListApp.swift
+//  SwipeActionToAnyList
+//
+//  Created by jirakit on 8/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwipeActionToAnyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
