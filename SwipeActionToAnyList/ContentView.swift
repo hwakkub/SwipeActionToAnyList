7f5f76f72512751e3d2ceed72edc4b85bc79f650
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-08 10:46:14 +0700

    Swipe (left-right)

--- SwipeActionToAnyList/ContentView.swift
+++ SwipeActionToAnyList/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var nameList = ["Ant","Bird","Cat","Dog"]
+     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                ForEach(nameList,id: \.self) {name in
+                    Text(name)
+                        .padding()
+                        .swipeActions{
+                            Button(role: .destructive){
+                                
+                            } label: {
+                                Label("Delete", systemImage: "trash.fill")
+                            }
+                        }
+                        .swipeActions{
+                            Button {
+                                
+                            } label: {
+                                Label("Edit", systemImage: "square.add.pencil")
+                            }.tint(.blue)
+                        }
+                        .swipeActions(edge:.leading) {
+                            Button {
+                                
+                            } label: {
+                                Label("Mic",systemImage: "mic.fill")
+                            }.tint(.green)
+                        }
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
